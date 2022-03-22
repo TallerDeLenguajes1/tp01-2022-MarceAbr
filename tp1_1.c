2e9e245b7fd8e9f8ef7774ed8392d5d876a606dd
Marcelo <marceloooabregu015@gmail.com>
2022-03-22 10:42:35 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,18 @@
 
 int main(){
 
+    int *punt, var;
+
     printf("Hola mundo");
 
+    var = 5;
+    punt = &var;
+
+    printf("\nEl contenido del puntero es: %d", *punt);
+    printf("\nDireccion de memoria almacenada por el puntero: %d", punt); 
+    printf("\nDireccion de memoria de la varible %d", &var);
+    printf("\nDireccion de memoria del puntero: %d", &punt);
+    printf("\nTamanio de memoria utilizado por la variable: %d", sizeof(var));
     return 0;
+
 }
